79910d2896c5dadda0e413138e3b555d0833bba2
geekahmed <geekahmed1@gmail.com>
2020-04-17 17:37:37 +0200

    Chapter2

---
+++ Chapter2-Program-Structure/FizzBuzz/main.js
@@ -0,0 +1,26 @@
+// First Version
+
+for (let i = 1 ; i <= 100 ; i++){
+  if (i % 3 == 0 ){
+    console.log('Fizz');
+  } else if (i % 5 == 0){
+    console.log('Buzz');
+  } else {
+    console.log(i);
+  }
+}
+
+//Second Version
+for (let i = 1 ; i <= 100 ; i++){
+  if (i % 3 == 0 && i % 5 == 0){
+    console.log('FizzBuzz'); 
+  } 
+    if (i % 3 == 0 ){
+      console.log('Fizz');
+    } else if (i % 5 == 0){
+      console.log('Buzz');
+    } else {
+      console.log(i);
+    }
+  }
+ 
